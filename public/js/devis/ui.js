0504997c15edb3d0817b3f79b8d9ba2caf21f2c0
Curti <yanncurti@gmail.com>
2025-10-07 12:42:35 +0200

    chore: remove duplicates via thin proxies/adapters; single source of truth for UI & recap 2

--- public/js/devis/ui.js
+++ public/js/devis/ui.js
@@ -1,3 +1,2 @@
-// public/js/devis/ui.js
-export { initDevis }   from '/js/devis/ui/index.js';
-export { resetDevis }  from '/js/devis/ui/reset.js';
+export { initDevis }  from '/js/devis/ui/index.js';
+export { resetDevis } from '/js/devis/ui/reset.js';
